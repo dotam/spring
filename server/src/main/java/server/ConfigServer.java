2a4caf9fb5633e92e50e3d7ef0dbf084c2a1a53f
tam <tam.do@vincere.io>
2018-06-12 09:15:49 +0700

    first commit

---
+++ server/src/main/java/server/ConfigServer.java
@@ -0,0 +1,18 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServer {
+
+    public static void main(String[] arguments) {
+        SpringApplication.run(ConfigServer.class, arguments);
+    }
+}
